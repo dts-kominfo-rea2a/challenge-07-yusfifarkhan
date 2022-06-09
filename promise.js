e1fbc5b220a3e5d005c8f677115aec3a369644e5
yusfifarkhan <93718536+yusfifarkhan@users.noreply.github.com>
2022-06-09 22:03:07 +0700

    Submit #1
    Challenge Promise membuat fungsi async/await untuk memfilter hasil promise dengan parameter emosi

--- promise.js
+++ promise.js
@@ -1,7 +1,36 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    /* Await hasil await promisenya */
+
+    const hasilIXX = await promiseTheaterIXX();
+    const hasilVGC = await promiseTheaterVGC();
+
+    /* End hasil await promisenya */
+
+    /* Hasil filter berdasarkan param emosi */
+
+    const hasilFilterEmosiIXX = hasilIXX.filter(
+      (judulFilm) => judulFilm.hasil === emosi
+    );
+
+    const hasilFilterEmosiVGC = hasilVGC.filter(
+      (judulFilm) => judulFilm.hasil === emosi
+    );
+
+    /* End Filter hasil promisenya */
+
+    /* Mengembalikan resut hasil promisenya */
+
+    return hasilFilterEmosiIXX.length + hasilFilterEmosiVGC.length;
+
+    /* End Mengembalikan resut hasil promisenya */
+  } catch (error) {
+    return `Ada error di ${error}`;
+  }
+};
 
 module.exports = {
   promiseOutput,
